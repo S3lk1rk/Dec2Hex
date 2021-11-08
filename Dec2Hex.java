b2947511182dd9a8925ba104673be21e1922423b
S3lk1rk <matthew.selkirk51@gmail.com>
2021-11-08 23:49:48 +0000

    testing invalid entry catch

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,21 +3,12 @@ import java.util.Scanner;
 class Dec2Hex
 {
 
-    public static Integer Arg1;
+    public static int Arg1;
 
     public static void main(String args[])
     {
-	Arg1 = Integer.parseInt(args[0]);
-       	if(Arg1 == null)
-
-	{
-            System.out.println("No input recorded"); 
-        } 
-        else
-        {
-
-         Arg1 = Integer.parseInt(args[0]);
-  
+        try {
+        Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
@@ -25,13 +16,20 @@ class Dec2Hex
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
-        	{
-            	rem=num%16;
-            	hexadecimal= ch[rem] + hexadecimal;
-            	num= num/16;
-        	}
-	
-        	System.out.println("Hexadecimal representation is: " + hexadecimal);
-    	}
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
+        }
+
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+            // Catch block to handle NumberFormatException
+            catch (NumberFormatException e) {
+ 
+                // Print the message if exception occured
+                System.out.println(
+                    "NumberFormatException occured");
+            }
     }
+
 }
