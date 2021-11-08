528474b07f9d1673e093cdb9b23602c0bdff8980
S3lk1rk <matthew.selkirk51@gmail.com>
2021-11-08 23:18:18 +0000

    testing invalid entry catch

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,4 +1,4 @@
-mport java.util.Scanner;
+import java.util.Scanner;
 
 class Dec2Hex
 {
@@ -7,8 +7,9 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        if(Arg1 == null)
+       
         Arg1 = Integer.parseInt(args[0]);
+	if(Arg1 == null)
 
 	{
             System.out.println("No input recorded"); 
@@ -24,13 +25,13 @@ class Dec2Hex
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-    }
+        	{
+            	rem=num%16;
+            	hexadecimal= ch[rem] + hexadecimal;
+            	num= num/16;
+        	}
+	
+        	System.out.println("Hexadecimal representation is: " + hexadecimal);
+    	}
     }
 }
