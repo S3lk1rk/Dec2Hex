bcb4444366a74d8623a63bc07df23356901d83c0
S3lk1rk <matthew.selkirk51@gmail.com>
2021-11-09 17:40:46 +0000

    testing exception handling for an empty file 6

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -9,7 +9,7 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        if (args.length = 0){
+        if (args.length == 0){
             System.out.println("INVALID");
             }
             try
