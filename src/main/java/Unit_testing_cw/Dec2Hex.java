c13e823e8952880b310c1217ecec7483a07c02c2
S3lk1rk <matthew.selkirk51@gmail.com>
2021-11-10 13:57:12 +0000

    uploading gradle to the main branch and my first unit test

---
+++ src/main/java/Unit_testing_cw/Dec2Hex.java
@@ -0,0 +1,54 @@
+package Unit_testing_cw;
+import java.util.Scanner;
+
+
+class Dec2Hex
+{
+
+    public static int EnteredValue;
+
+    public static void main(String args[])
+    {
+
+     String result = Dec2HexConv(args);
+	System.out.println(result);
+
+    }
+    public static String Dec2HexConv(String args[]){
+
+        if (args.length == 0 || (args.equals(" "))){
+
+                String empty= "No entry made, please enter an integer";
+                return empty;
+            }
+            try
+            {
+                 EnteredValue = Integer.parseInt(args[0]);
+            }
+            catch(NumberFormatException ex)
+            {
+                String exception="The entered value exists outwith the current bounds of the system, please enter an integer";
+            return exception;
+                }
+
+
+
+        EnteredValue = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = EnteredValue;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
+        }
+
+        String conversion= ("Hexadecimal representation is: " + hexadecimal);
+        return conversion;
+}
+}
+
